ae46d81b72f8312364fc91b8a4dcd3e0fb0a3188
mroy-25 <78289020+mroy-25@users.noreply.github.com>
2021-03-18 01:35:03 +0000

    Add files via upload

---
+++ teks/help.js
@@ -0,0 +1,83 @@
+exports.Help = (prefix) => {
+    return `   「 *SELEP BOT* 」
+
+◪ *INFO*
+❏ *Ver :* Baileys
+❏ *Prefix :* 「 ${prefix} 」
+❏ Creator : MRoy25
+					
+◪ *GROUP*
+  │
+  ├ ❏ ${prefix}hidetag <text>
+  ├ ❏ ${prefix}gruop close|open
+  ├ ❏ ${prefix}gcname <text>
+  ├ ❏ ${prefix}gcdesk <text>
+  ├ ❏ ${prefix}linkgroup
+  ├ ❏ ${prefix}add 62xxx
+  ├ ❏ ${prefix}kick tag/62xxx
+  ├ ❏ ${prefix}tagall
+  ├ ❏ ${prefix}ownergc
+  ├ ❏ ${prefix}leave	
+  ├ ❏ ${prefix}promote
+  ├❏ ${prefix}demote
+  ├❏ ${prefix}antilink enable/disable
+  └ END
+				
+◪ *MEDIA*
+  │
+  ├ ❏ ${prefix}spamcall 08xxx
+  ├ ❏ ${prefix}spamsms 08xxx
+  ├ ❏ ${prefix}joox <optional>
+  ├ ❏ ${prefix}play <optional>
+  ├ ❏ ${prefix}mp3 <link yt>
+  ├ ❏ ${prefix}mp4 <link yt>
+  ├ ❏ ${prefix}gsearch <otional>
+  ├ ❏ ${prefix}ssweb <link>					  
+  ├ ❏ ${prefix}sswebfull <link>
+  ├ ❏ ${prefix}igstalk <optional>
+  ├ ❏ ${prefix}ig <link>
+  ├ ❏ ${prefix}fb <link>
+  ├ ❏ ${prefix}twitter <link>
+  ├ ❏ ${prefix}twm <link>
+  ├ ❏ ${prefix}tnown <link>
+  ├ ❏ ${prefix}tmusic <link>
+  ├ ❏ ${prefix}tahta <text>
+  ├ ❏ ${prefix}ttp <text>
+  ├ ❏ ${prefix}fitnah <text|teks>
+  ├ ❏ ${prefix}pinterest <optional>
+  ├ ❏ ${prefix}pixiv <optional>
+  ├ ❏ ${prefix}konachan <optional>
+  ├ ❏ ${prefix}bitly <linknya>  
+  ├ ❏ ${prefix}shortlink <linknya>
+  ├ ❏ ${prefix}imgtourl <linknya>
+  ├ ❏ ${prefix}sticker <reply/tag gambar>  
+  ├ ❏ ${prefix}toimg <reply sticker>					  
+  ├ ❏ ${prefix}tomp3 <reply video>
+  ├ ❏ ${prefix}toptt <reply audio>
+  ├ ❏ ${prefix}memeimg <teks|teks>
+  ├ ❏ ${prefix}meme
+  ├ ❏ ${prefix}wait
+  ├ ❏ ${prefix}loli
+  ├ ❏ ${prefix}waifu
+  └ END
+					
+					
+◪ *ADVANCED*
+  │
+  ├ ❏ ${prefix}setpict <tag/reply gambar>
+  ├ ❏ ${prefix}setnick <optional>  
+  ├ ❏ ${prefix}setnomor <nomor>					  
+  ├ ❏ ${prefix}setpesan <pesan>
+  ├ ❏ ${prefix}setprefix <symbol>
+  ├ ❏ ${prefix}clearall
+  ├ ❏ ${prefix}delete
+  ├ ❏ ${prefix}fake <text>
+  ├ ❏ ${prefix}fordward <text>
+  ├ ❏ ${prefix}kontag 62xxx
+  ├ ❏ ${prefix}kontak 62xxx
+  ├ ❏ ${prefix}runtime
+  ├ ❏ ${prefix}ping
+  └ END
+					
+◩ *SELEP BOT*`
+}
